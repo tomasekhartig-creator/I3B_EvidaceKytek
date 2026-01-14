53538b6a37515219c88aefd1c143a996959a3ebb
Hartig <Tomasekhartig@gmail.com>
2026-01-14 11:55:53 +0100

    uprava

--- I3B_EvidaceKytek/MainWindow.xaml.cs
+++ I3B_EvidaceKytek/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using I3B_EvidaceKytek.Interfaces;
 using I3B_EvidaceKytek.Managers;
 using I3B_EvidaceKytek.models;
 using I3B_EvidaceKytek.Repositories;
+using I3B_EvidaceKytek.Windows;
 using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
@@ -22,14 +23,28 @@ namespace I3B_EvidaceKytek
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Flower> Data { get; set; }
         public IFlowerManager Manager { get; set; }
         public MainWindow()
         {
             IFlowerRepository repository = new FlowerRepository(new FlowerContext());
             Manager = new FlowerManager(repository);
+            Data = new ObservableCollection<Flower>(Manager.GetAll());
             InitializeComponent();
 
-            LV.ItemsSource = new ObservableCollection<Flower>(Manager.GetAll());
+            LV.ItemsSource = Data;
+        }
+
+        private void OnSaveClick(object sender, EventArgs e)
+        {
+            AddNewFlowerWindows AddWindow = new(Manager);
+            AddWindow.Closing += (s, e) =>
+            {
+                Data.Clear();   
+                 LV.ItemsSource = Data;
+            };
+
+            AddWindow.ShowDialog();
         }
     }
 }
---
+++ I3B_EvidaceKytek/Windows/AddNewFlowerWindows.xaml.cs
@@ -0,0 +1,63 @@
+﻿using I3B_EvidaceKytek.Interfaces;
+using I3B_EvidaceKytek.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace I3B_EvidaceKytek.Windows
+{
+    /// <summary>
+    /// Interakční logika pro AddNewFlowerWindows.xaml
+    /// </summary>
+    public partial class AddNewFlowerWindows : Window
+    {
+        IFlowerManager _manager;
+        public AddNewFlowerWindows(IFlowerManager manager)
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Flower newFlower = getFlower();
+                _manager.Add(newFlower);
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private Flower getFlower()
+        {
+            string name = NameInput.Text;
+            string genus = GenusInput.Text;
+            string color = ColorInput.SelectedValue.ToString();
+            if(name.Length > 2 || genus.Length > 2 || ColorInput.SelectedIndex  != -1)
+            {
+                return new Flower { Name = name,
+                    Genus = genus,
+                    Color = color
+                };
+            }
+            throw new Exception("The input data are incorrect!");
+            return new Flower { Name = name,
+                Genus = genus,
+                Color = color
+            };
+        }
+    }
+}
